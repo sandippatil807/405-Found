8c901f1fb3b283ae212a08cd2e0ccbf98e7b32e9
Sandip Patil <50192249+sandippatil807@users.noreply.github.com>
2021-06-27 19:29:27 +0530

    Update rsp.js

--- Beautify/rsp.js
+++ Beautify/rsp.js
@@ -13,6 +13,21 @@ module.exports = {
     }
   }
   /*
+  module.exports = {
+    theme: {
+      screens: {
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    }
+  }
+  
   class MyComponent extends HTMLElement {
     constructor() {
       super();
